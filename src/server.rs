openresearchtools/virtiofsd

commit 5c9961d1eab7b38e466b3a2774ef9c3a8bb5c2b7

Use fuse::Attr in FileSystem interfaces
In the FileSystem interface, replace libc::stat64 by fuse::Attr.

With UID/GID mapping, converting one to the other is going to be
fallible, so server.rs can no longer do it (because generating guest
errors there is cumbersome and ugly), and we would rather have the
filesystem driver (PassthroughFs) attempt the conversion.

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/server.rs
@@ -239,12 +239,12 @@ impl<F: FileSystem + Sync> Server<F> {
             .fs
             .getattr(Context::from(in_header), in_header.nodeid.into(), handle)
         {
-            Ok((st, timeout)) => {
+            Ok((attr, timeout)) => {
                 let out = AttrOut {
                     attr_valid: timeout.as_secs(),
                     attr_valid_nsec: timeout.subsec_nanos(),
                     dummy: 0,
-                    attr: st.into(),
+                    attr,
                 };
                 reply_ok(Some(out), None, in_header.unique, w)
             }
@@ -263,21 +263,19 @@ impl<F: FileSystem + Sync> Server<F> {
 
         let valid = SetattrValid::from_bits_truncate(setattr_in.valid);
 
-        let st: libc::stat64 = setattr_in.into();
-
         match self.fs.setattr(
             Context::from(in_header),
             in_header.nodeid.into(),
-            st,
+            setattr_in,
             handle,
             valid,
         ) {
-            Ok((st, timeout)) => {
+            Ok((attr, timeout)) => {
                 let out = AttrOut {
                     attr_valid: timeout.as_secs(),
                     attr_valid_nsec: timeout.subsec_nanos(),
                     dummy: 0,
-                    attr: st.into(),
+                    attr,
                 };
                 reply_ok(Some(out), None, in_header.unique, w)
             }
@@ -1047,16 +1045,15 @@ impl<F: FileSystem + Sync> Server<F> {
         let entry = if name == CURRENT_DIR_CSTR || name == PARENT_DIR_CSTR {
             // Don't do lookups on the current directory or the parent directory. Safe because
             // this only contains integer fields and any value is valid.
-            let mut attr = unsafe { MaybeUninit::<libc::stat64>::zeroed().assume_init() };
-            attr.st_ino = dir_entry.ino;
-            attr.st_mode = dir_entry.type_ << 12;
+            let mut attr = unsafe { MaybeUninit::<Attr>::zeroed().assume_init() };
+            attr.ino = dir_entry.ino;
+            attr.mode = dir_entry.type_ << 12;
 
             // We use 0 for the inode value to indicate a negative entry.
             Entry {
                 inode: 0,
                 generation: 0,
                 attr,
-                attr_flags: 0,
                 attr_timeout: Duration::from_secs(0),
                 entry_timeout: Duration::from_secs(0),
             }
@@ -1261,15 +1258,7 @@ impl<F: FileSystem + Sync> Server<F> {
             extensions,
         ) {
             Ok((entry, handle, opts)) => {
-                let entry_out = EntryOut {
-                    nodeid: entry.inode,
-                    generation: entry.generation,
-                    entry_valid: entry.entry_timeout.as_secs(),
-                    attr_valid: entry.attr_timeout.as_secs(),
-                    entry_valid_nsec: entry.entry_timeout.subsec_nanos(),
-                    attr_valid_nsec: entry.attr_timeout.subsec_nanos(),
-                    attr: Attr::with_flags(entry.attr, entry.attr_flags),
-                };
+                let entry_out = EntryOut::from(entry);
                 let open_out = OpenOut {
                     fh: handle.map(Into::into).unwrap_or(0),
                     open_flags: opts.bits(),

src/filesystem.rs
@@ -32,10 +32,7 @@ pub struct Entry {
     /// Inode attributes. Even if `attr_timeout` is zero, `attr` must be correct. For example, for
     /// `open()`, FUSE uses `attr.st_size` from `lookup()` to determine how many bytes to request.
     /// If this value is not correct, incorrect data will be returned.
-    pub attr: libc::stat64,
-
-    /// Flags for `fuse::Attr.flags`.
-    pub attr_flags: u32,
+    pub attr: fuse::Attr,
 
     /// How long the values in `attr` should be considered valid. If the attributes of the `Entry`
     /// are only modified by the FUSE client, then this should be set to a very large value.
@@ -56,7 +53,7 @@ impl From<Entry> for fuse::EntryOut {
             attr_valid: entry.attr_timeout.as_secs(),
             entry_valid_nsec: entry.entry_timeout.subsec_nanos(),
             attr_valid_nsec: entry.attr_timeout.subsec_nanos(),
-            attr: fuse::Attr::with_flags(entry.attr, entry.attr_flags),
+            attr: entry.attr,
         }
     }
 }
@@ -471,7 +468,7 @@ pub trait FileSystem {
         ctx: Context,
         inode: Self::Inode,
         handle: Option<Self::Handle>,
-    ) -> io::Result<(libc::stat64, Duration)> {
+    ) -> io::Result<(fuse::Attr, Duration)> {
         Err(io::Error::from_raw_os_error(libc::ENOSYS))
     }
 
@@ -496,10 +493,10 @@ pub trait FileSystem {
         &self,
         ctx: Context,
         inode: Self::Inode,
-        attr: libc::stat64,
+        attr: fuse::SetattrIn,
         handle: Option<Self::Handle>,
         valid: SetattrValid,
-    ) -> io::Result<(libc::stat64, Duration)> {
+    ) -> io::Result<(fuse::Attr, Duration)> {
         Err(io::Error::from_raw_os_error(libc::ENOSYS))
     }
 

src/passthrough/mod.rs
@@ -851,14 +851,14 @@ impl PassthroughFs {
             self.inodes.get_or_insert(inode_data)?
         };
 
+        let attr = fuse::Attr::with_flags(st.st, attr_flags);
         Ok(Entry {
             // By leaking, we transfer ownership of this refcount to the guest.  That is safe,
             // because the guest is expected to explicitly release its reference and decrement the
             // refcount via `FORGET` later.
             inode: unsafe { inode.leak() },
             generation: 0,
-            attr: st.st,
-            attr_flags,
+            attr,
             attr_timeout: self.cfg.attr_timeout,
             entry_timeout: self.cfg.entry_timeout,
         })
@@ -944,12 +944,13 @@ impl PassthroughFs {
         Err(ebadf())
     }
 
-    fn do_getattr(&self, inode: Inode) -> io::Result<(libc::stat64, Duration)> {
+    fn do_getattr(&self, inode: Inode) -> io::Result<(fuse::Attr, Duration)> {
         let data = self.inodes.get(inode).ok_or_else(ebadf)?;
         let inode_file = data.get_file()?;
         let st = statx(&inode_file, None)?.st;
+        let attr = st.into();
 
-        Ok((st, self.cfg.attr_timeout))
+        Ok((attr, self.cfg.attr_timeout))
     }
 
     fn do_unlink(&self, parent: Inode, name: &CStr, flags: libc::c_int) -> io::Result<()> {
@@ -1782,7 +1783,7 @@ impl FileSystem for PassthroughFs {
         _ctx: Context,
         inode: Inode,
         _handle: Option<Handle>,
-    ) -> io::Result<(libc::stat64, Duration)> {
+    ) -> io::Result<(fuse::Attr, Duration)> {
         self.do_getattr(inode)
     }
 
@@ -1790,10 +1791,11 @@ impl FileSystem for PassthroughFs {
         &self,
         _ctx: Context,
         inode: Inode,
-        attr: libc::stat64,
+        attr: fuse::SetattrIn,
         handle: Option<Handle>,
         valid: SetattrValid,
-    ) -> io::Result<(libc::stat64, Duration)> {
+    ) -> io::Result<(fuse::Attr, Duration)> {
+        let attr = libc::stat64::from(attr);
         let inode_data = self.inodes.get(inode).ok_or_else(ebadf)?;
 
         // In this case, we need to open a new O_RDWR FD

commit c5b591953b0c88ea2b10923884843df231d7cebc

handle_dirent: Fill in fuse::Attr by Default
Instead of pre-filling unset `Attr` fields via `MaybeUninit::zeroed()`,
use `..Default::default()`.

This is an artifact from when we created a `libc::stat64` object here,
which does not implement `Default`; `fuse::Attr` does, however, so we
can use it.

Fixes: 5c9961d1eab7b38e466b3a2774ef9c3a8bb5c2b7
       (“Use fuse::Attr in FileSystem interfaces”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/server.rs
@@ -15,7 +15,7 @@ use std::convert::{TryFrom, TryInto};
 use std::ffi::{CStr, CString};
 use std::fs::File;
 use std::io::{self, Read, Write};
-use std::mem::{size_of, MaybeUninit};
+use std::mem::size_of;
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
@@ -1058,17 +1058,17 @@ impl<F: FileSystem + Sync> Server<F> {
         let parent = in_header.nodeid.into();
         let name = dir_entry.name.to_bytes();
         let entry = if name == CURRENT_DIR_CSTR || name == PARENT_DIR_CSTR {
-            // Don't do lookups on the current directory or the parent directory. Safe because
-            // this only contains integer fields and any value is valid.
-            let mut attr = unsafe { MaybeUninit::<Attr>::zeroed().assume_init() };
-            attr.ino = dir_entry.ino;
-            attr.mode = dir_entry.type_ << 12;
-
             // We use 0 for the inode value to indicate a negative entry.
             Entry {
                 inode: 0,
                 generation: 0,
-                attr,
+                // Don't do lookups on the current directory or the parent directory, i.e. leave
+                // most fields 0.
+                attr: Attr {
+                    ino: dir_entry.ino,
+                    mode: dir_entry.type_ << 12,
+                    ..Default::default()
+                },
                 attr_timeout: Duration::from_secs(0),
                 entry_timeout: Duration::from_secs(0),
             }